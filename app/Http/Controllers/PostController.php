8462d330b16669aea25d21581473ef4c9a5440f4
Marissa belle <marissabellebindeki@gmail.com>
2026-02-23 16:41:56 -0500

    refactor use post controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): Factory|\Illuminate\Contracts\View\View
+    {
+        $posts = Post::all();
+        return view('posts.index', ['posts' => $posts]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): View
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate([
+            "title" => "required|max:255",
+            "description" => "nullable|min:10",
+        ]);
+        Post::query()->create($validated);
+        return redirect('/posts');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $posts): View
+    {
+        return view('posts.show', ['post' => $posts]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $posts): View
+    {
+        return view('posts.edit', ['post' => $posts]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $posts): RedirectResponse
+    {
+        $validated = $request->validate([
+            "title" => "required|max:255",
+            "description" => "nullable|min:10",
+        ]);
+        $posts->update($validated);
+        return redirect('/posts');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $posts): RedirectResponse
+    {
+        $posts->delete();
+        return redirect('/posts');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\TestContoller;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+
+
 
 //quand l'utilisateur veut accerder a la page dacceuil (les routes qui sont acceder par des routeur)
 //methode http:
@@ -12,53 +16,55 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', function () {
-    $posts = Post::all();
-    return view('posts.index', [
-        'posts' => $posts
-    ]);
-});
+Route::resource("posts",PostController::class);
 
-Route::post("/posts", function (Request $request) {
-   $validated = $request->validate([
-       'title' => 'required|max:255',
-       "description" => "nullable|min:10"
-   ]);
-
-   Post::query()->create($validated);
-
-    return redirect("/posts");
-});
-
-
-
-//mon php artisan serve est different de composer run dev
-// tous ce qui est entre les acolades on appelle ca un  segment
-//ls vaiables qui sont dans les accollades doivent etre du meme nomque la variables qui est dans string
-Route::get('/posts/{id}', function (string $id) {
-$post = Post::query()->findOrFail($id);
-return view('posts.show', ["post" => $post]);
-})->whereNumber('id');
-
-Route:: get("/posts/{id}/edit", function (string $id) {
-    $post = post::query()->findOrFail($id);
-    return view('posts.edit', ["post" => $post]);
-});
-
-Route::put("/posts/{id}", function (Request $request, string $id) {
-    $validated = $request->validate([
-        'title' => 'required|max:255',
-        "description" => "nullable|min:10"
-    ]);
-        Post::query()->findOrFail($id)->update($validated);
-    return redirect("/posts/$id");
-});
-
-Route::delete('/posts/{id}', function (string $id) {
-    post::query()->findOrFail($id)->delete();
-    return redirect("/posts");
-});
-
-Route::get('/posts/create', function () {
-    return view('posts.create');
-});
+////Route::get("/posts", [PostController::class, 'index']);
+////Route::get("/posts/{post}", [PostController::class, 'show']);
+////Route::get('/posts', function () {
+////    $posts = Post::all();
+////    return view('posts.index', [
+////        'posts' => $posts
+////    ]);
+////});
+//
+//Route::post("/posts", function (Request $request) {
+//   $validated = $request->validate([
+//       'title' => 'required|max:255',
+//       "description" => "nullable|min:10"
+//   ]);
+//
+//   Post::query()->create($validated);
+//
+//    return redirect("/posts");
+//});
+//
+//
+//
+////mon php artisan serve est different de composer run dev
+//// tous ce qui est entre les acolades on appelle ca un  segment
+////ls vaiables qui sont dans les accollades doivent etre du meme nomque la variables qui est dans string
+//Route::get('/posts/{post}', function (string $post) {
+//return view('posts.show', ["post" => $post]);
+//});
+//
+//Route:: get("/posts/{post}/edit", function (string $post) {
+//    return view('posts.edit', ["post" => $post]);
+//});
+//
+//Route::put("/posts/{post}", function (Request $request, string $post) {
+//    $validated = $request->validate([
+//        'title' => 'required|max:255',
+//        "description" => "nullable|min:10"
+//    ]);
+//        $post->update($validated);
+//    return redirect("/posts/$post->id");
+//});
+//
+//Route::delete('/posts/{post}', function (string $post) {
+//    post::query()->findOrFail($post)->delete();
+//    return redirect("/posts");
+//});
+//
+//Route::get('/posts/create', function () {
+//    return view('posts.create');
+//});
